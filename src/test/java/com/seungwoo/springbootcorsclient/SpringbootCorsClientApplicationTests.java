973aa19c09bf9e2b429a67b962dada068d202a9f
SONSEUNGWOO <skyghy3342@adwitt.com>
2018-05-22 15:41:07 +0900

    Initial commit

---
+++ src/test/java/com/seungwoo/springbootcorsclient/SpringbootCorsClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.seungwoo.springbootcorsclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootCorsClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
